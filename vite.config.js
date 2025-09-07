b51f9f5a8d7f3975b4ca7d6cd5068cb8370b55e3
sizimi920 <sizimi920@gmail.com>
2025-09-07 13:09:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-     base: '/react-quiz/',   // 👈 リポジトリ名に合わせる
+     base: '/react-quiz1/',   // 👈 リポジトリ名に合わせる
     plugins: [react()],
 })
