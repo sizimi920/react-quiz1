f89228b454743aeaabc3bf5e41085ea31435f833
sizimi920 <sizimi920@gmail.com>
2025-09-27 00:27:53 +0900

    fix: correct syntax for exporting Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig() => {
-    return {
-        server: {
-            open: "/"
-        },
-        base: "/react-quiz/", // build時だけ /quiz/
-        plugins: [react()],
-    }
-})
+export default defineConfig({
+    server: {
+        open: "/"
+    },
+    base: "/react-quiz/", // build時だけ /quiz/
+    plugins: [react()],
+});
